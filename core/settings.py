84d93774a8b9197c3c8a7d74010a465d82fec1f3
fsp3012 <fakoredeabdulafeezolamide@gmail.com>
2023-08-10 15:18:59 +0100

    updated

--- core/settings.py
+++ core/settings.py
@@ -72,7 +72,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
+    'http://localhost:5174',
     'http://localhost:3000',
     'https://comforting-souffle-589f2e.netlify.app',
 ]
